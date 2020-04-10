2066648e6c7b5319ae2fa62742fa81a028fecf01
coolcao <me@coolcao.com>
2020-04-10 12:01:42 +0800

    update 189.rotate-array.go

--- src/go/189.rotate-array.go
+++ src/go/189.rotate-array.go
@@ -1,59 +1,58 @@
 /*
  * @lc app=leetcode id=189 lang=golang
- *
  * [189] Rotate Array
- */
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+
+难度：简单
+*/
 
 package main
 
-import "fmt"
-
 // @lc code=start
 func rotate(nums []int, k int) {
-	for k > 7 {
-		k = k % 7
+	length := len(nums)
+	if k >= length {
+		k = k % length
 	}
 	if k == 0 {
 		return
 	}
-	length := len(nums)
-	idx := length - 1
-	value := nums[idx]
-	for i := 0; i < k; i++ {
-		value = nums[idx]
-		for j := length - 1; j > 0; j-- {
-			nums[j] = nums[j-1]
-		}
-		nums[0] = value
-	}
-}
-
-func rotate2(nums []int, k int) {
-	for k > 7 {
-		k = k % 7
-	}
-	if k == 0 {
-		return
-	}
-	length := len(nums)
 	tmp := make([]int, k)
 	for i := 0; i < k; i++ {
-		tmp[i] = nums[length-1-i]
+		tmp[i] = nums[length-k+i]
 	}
-	for i := length - 1; i >= 0; i-- {
-		if i >= k {
-			nums[i] = nums[i-k]
-		} else {
-			nums[i] = tmp[k-1-i]
-		}
+	for i := length - 1; i >= k; i-- {
+		nums[i], nums[i-k] = nums[i-k], nums[i]
+	}
+	for i := 0; i < k; i++ {
+		nums[i] = tmp[i]
 	}
 }
 
-func main() {
-	nums := []int{1, 2, 3, 4, 5, 6, 7}
-	k := 8
-	rotate2(nums, k)
-	fmt.Printf("%v\n", nums)
-}
+// func main() {
+// 	nums := []int{1, 2, 3, 4, 5, 6, 7}
+// 	k := 3
+// 	rotate(nums, k)
+// }
 
 // @lc code=end
