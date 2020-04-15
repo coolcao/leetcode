d89f9fa88c90652b0bcde422d22fe1da3a0fdaf9
coolcao <me@coolcao.com>
2020-04-15 23:16:00 +0800

    add 443

---
+++ src/go/443.string-compression.go
@@ -0,0 +1,151 @@
+/*
+ * @lc app=leetcode id=443 lang=golang
+ *
+ * [443] String Compression
+ *
+ * https://leetcode.com/problems/string-compression/description/
+ *
+ * algorithms
+ * Easy (40.09%)
+ * Likes:    661
+ * Dislikes: 2031
+ * Total Accepted:    95.2K
+ * Total Submissions: 237.2K
+ * Testcase Example:  '["a","a","b","b","c","c","c"]'
+ *
+ * Given an array of characters, compress it in-place.
+ *
+ * The length after compression must always be smaller than or equal to the
+ * original array.
+ *
+ * Every element of the array should be a character (not int) of length 1.
+ *
+ * After you are done modifying the input array in-place, return the new length
+ * of the array.
+ *
+ *
+ * Follow up:
+ * Could you solve it using only O(1) extra space?
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input:
+ * ["a","a","b","b","c","c","c"]
+ *
+ * Output:
+ * Return 6, and the first 6 characters of the input array should be:
+ * ["a","2","b","2","c","3"]
+ *
+ * Explanation:
+ * "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by
+ * "c3".
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input:
+ * ["a"]
+ *
+ * Output:
+ * Return 1, and the first 1 characters of the input array should be: ["a"]
+ *
+ * Explanation:
+ * Nothing is replaced.
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input:
+ * ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+ *
+ * Output:
+ * Return 4, and the first 4 characters of the input array should be:
+ * ["a","b","1","2"].
+ *
+ * Explanation:
+ * Since the character "a" does not repeat, it is not compressed.
+ * "bbbbbbbbbbbb" is replaced by "b12".
+ * Notice each digit has it's own entry in the array.
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * All characters have an ASCII value in [35, 126].
+ * 1 <= len(chars) <= 1000.
+ *
+ *
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	chars := []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c'}
+	c := compress(chars)
+	fmt.Printf("%c:%d\n", chars, c)
+}
+
+// @lc code=start
+func compress(chars []byte) int {
+	length := len(chars)
+	if length <= 1 {
+		return length
+	}
+
+	pre := chars[0]
+	count := 1
+	idx := 0
+	for i := 1; i < length; i++ {
+		if chars[i] == pre {
+			count++
+		} else {
+			if count == 1 {
+				chars[idx] = pre
+				idx++
+			} else {
+				chars[idx] = pre
+				for count > 9 {
+					chars[idx+1] = byte(count/10 + 48)
+					idx++
+					count %= 10
+				}
+				chars[idx+1] = byte(count + 48)
+				idx += 2
+			}
+
+			pre = chars[i]
+			count = 1
+
+		}
+	}
+
+	if count == 1 {
+		chars[idx] = pre
+		idx++
+	} else {
+		chars[idx] = pre
+		for count > 9 {
+			chars[idx+1] = byte(count/10 + 48)
+			idx++
+			count %= 10
+		}
+		chars[idx+1] = byte(count + 48)
+		idx += 2
+	}
+
+	return idx
+}
+
+// @lc code=end
