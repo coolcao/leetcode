b7f9123c405311075f4d5500b0f2c191753a2b61
coolcao <me@coolcao.com>
2020-04-16 11:41:30 +0800

    update 447

--- 447.number-of-boomerangs.go
+++ 447.number-of-boomerangs.go
@@ -38,10 +38,12 @@
  */
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	points := [][]int{[]int{0, 0}, []int{2, 0}, []int{1, 0}, []int{3, 0}}
+	points := [][]int{[]int{0, 0}, []int{2, 0}, []int{1, 0}, []int{3, 0}, []int{1, 1}}
 	c := numberOfBoomerangs(points)
 	numberOfBoomerangs2(points)
 	fmt.Printf("count: %d\n", c)
@@ -49,6 +51,10 @@ func main() {
 }
 
 // @lc code=start
+func distance(pi, pj []int) int {
+	return (pi[0]-pj[0])*(pi[0]-pj[0]) + (pi[1]-pj[1])*(pi[1]-pj[1])
+}
+
 // 方法1: 穷举，把所有可能都列举出来，思路简单，效率低
 func numberOfBoomerangs(points [][]int) int {
 	length := len(points)
@@ -63,7 +69,7 @@ func numberOfBoomerangs(points [][]int) int {
 					continue
 				}
 				pi, pj, px := points[i], points[j], points[x]
-				if (pi[0]-pj[0])*(pi[0]-pj[0])+(pi[1]-pj[1])*(pi[1]-pj[1]) == (pi[0]-px[0])*(pi[0]-px[0])+(pi[1]-px[1])*(pi[1]-px[1]) {
+				if distance(pi, pj) == distance(pi, px) {
 					count++
 				}
 			}
@@ -72,26 +78,27 @@ func numberOfBoomerangs(points [][]int) int {
 	return count
 }
 
+// 方法2，使用hash缓存每个点到其他点的距离，遇到距离相同的，统计
 func numberOfBoomerangs2(points [][]int) int {
 	length := len(points)
 	if length < 3 {
 		return 0
 	}
-
-	distances := make([][]int, length-1)
-
-	// 先把所有点的距离算出来
-	for i := 0; i < length-1; i++ {
-		tmp := make([]int, length)
-		for j := i + 1; j < length; j++ {
-			distance := (points[i][0]-points[j][0])*(points[i][0]-points[j][0]) + (points[i][1]-points[j][1])*(points[i][1]-points[j][1])
-			tmp[j] = distance
-			// fmt.Printf("i:%d,j:%d,distance: %d\n", i, j, distance)
+	count := 0
+	for i := 0; i < length; i++ {
+		disMap := map[int]int{}
+		for j := 0; j < length; j++ {
+			if i != j {
+				dist := distance(points[i], points[j])
+				// 这个距离出现过，累计回旋标的个数
+				if disMap[dist] > 0 {
+					count += disMap[dist] * 2
+				}
+				disMap[dist]++
+			}
 		}
-		distances[i] = tmp
 	}
-	fmt.Printf("%v\n", distances)
-	return 0
+	return count
 }
 
 // @lc code=end
