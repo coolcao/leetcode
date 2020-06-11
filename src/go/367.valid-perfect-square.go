9556e2635972bf8b3742e1f188d316dd166e9c48
曹洪升 <caohongsheng@nequal.com>
2020-06-11 18:42:56 +0800

    add 367

--- src/go/367.valid-perfect-square.go
+++ src/go/367.valid-perfect-square.go
@@ -19,54 +19,25 @@
  * Note: Do not use any built-in library function such as sqrt.
  *
  * Example 1:
- *
- *
- *
  * Input: 16
  * Output: true
  *
  *
  *
  * Example 2:
- *
- *
  * Input: 14
  * Output: false
- *
- *
  *
  */
 package main
 
-import "fmt"
-
-func main() {
-	num := 334084
-	fmt.Println(isPerfectSquare(num))
-}
-
-/*
-问题：判断一个正整数是否是平方数。
-
-判断一个数是否是平方数，最简单的办法就是从1开始计算平方，如果正好相等，那就是平方数。
-但有一点需要注意，计算机中的整数，是有最大限制的，超过最大数会出现溢出。
-所以在循环判断时，要注意计算平方数的溢出情况。
-**/
-
 // @lc code=start
 func isPerfectSquare(num int) bool {
-	// int 最大值
-	INT_MAX := int(^uint(0) >> 1)
-	i := 1
-	isquare := i * i
-	for isquare <= num && INT_MAX-isquare >= 2*i+1 {
-		if isquare == num {
-			return true
-		}
-		i++
-		isquare = i * i
+	// 连续奇数的和即为完全平方数
+	for i := 1; num > 0; i += 2 {
+		num = num - i
 	}
-	return false
+	return num == 0
 }
 
 // @lc code=end
