7378788c02033fbdbc66462dbf7cee8c85dd8c7e
曹洪升 <caohongsheng@nequal.com>
2020-06-16 16:10:55 +0800

    add 559

---
+++ src/go/559.maximum-depth-of-n-ary-tree.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=559 lang=golang
+ *
+ * [559] Maximum Depth of N-ary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+ *
+ * algorithms
+ * Easy (67.66%)
+ * Likes:    759
+ * Dislikes: 44
+ * Total Accepted:    106K
+ * Total Submissions: 155.6K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * Given a n-ary tree, find its maximum depth.
+ *
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ *
+ * Nary-Tree input serialization is represented in their level order traversal,
+ * each group of children is separated by the null value (See examples).
+ *
+ *
+ * Example 1:
+ *
+ *
+ *
+ *
+ * Input: root = [1,null,3,2,4,null,5,6]
+ * Output: 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ *
+ *
+ * Input: root =
+ * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ * Output: 5
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The depth of the n-ary tree is less than or equal to 1000.
+ * The total number of nodes is between [0, 10^4].
+ *
+ *
+ */
+package main
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+// @lc code=start
+func visit(node *Node, level int) int {
+	if len(node.Children) == 0 {
+		return level
+	}
+	levels := []int{}
+	for _, n := range node.Children {
+		levels = append(levels, visit(n, level+1))
+	}
+	max := levels[0]
+	for _, l := range levels {
+		if l > max {
+			max = l
+		}
+	}
+	return max
+}
+func maxDepth(root *Node) int {
+	if root == nil {
+		return 0
+	}
+	return visit(root, 1)
+}
+
+// @lc code=end
